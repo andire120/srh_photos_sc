2d114f13f233b2750b0876f83daa3f850ddd2c11
JangSeung <0sunghee122@gmail.com>
2025-03-14 00:51:14 +0900

    .

--- front/src/screen/StartScreen.js
+++ front/src/screen/StartScreen.js
@@ -4,7 +4,16 @@ import "./StartScreen.css"; // Importing CSS for styles
 const StartScreen = ({ onStart }) => {
   return (
     <div className="start-screen">
-      <img src="./spamlogo.png" alt="Spam Logo" className="logo" />
+      <div>
+        <img src="./spamlogo.png" alt="Spam Logo" className="logo" />
+        <p className="jum">●</p>
+      </div>
+      <div>
+        <img src="./spamlogo2.png" alt="Spam Logo Ver2" className="logo2" />
+        <h2 className="big">Self Photo Booth</h2><h4 className="smail">of SRH</h4>
+      </div>
+
+      
       <button onClick={onStart}>시작하기</button>
     </div>
   );
