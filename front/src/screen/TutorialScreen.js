ecb6df6aeb5360adb2627facea55884f2c3cfc93
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2025-03-16 19:28:29 +0900

    끝났나
    수정할 무언가가 있으면 찔러

--- front/src/screen/TutorialScreen.js
+++ front/src/screen/TutorialScreen.js
@@ -4,23 +4,25 @@ import "./TutorialScreen.css";
 const TutorialScreen = ({ onComplete }) => {
     return(
         <>
-        <p className="head">HOW TO USE</p>
+        <h1 className="head">HOW TO USE</h1>
     <div class="container">
         
     <div class="left">
         <img src="example.png" alt="example" />
     </div>
-    <div class="right">
+    <div class="no-center">
     <ul>
+        <li>5초에 한번씩 네번의 사진이 촬영됩니다.<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;시간에 맞추어 포즈를 취해보세요!<br/><br/></li>
         <li>촬영 전 마음에 드는 프레임을 골라주세요.<br/><br/></li>
-        <li>5초에 한번씩 네번의 사진이 촬영됩니다.<br/>시간에 맞추어 포즈를 취해보세요!<br/></li>
-        <li>완성된 스팸네컷을 출력하거나 다운로드하여 <br/> 간직하세요.</li>
-</ul>
+        <li>완성된 스팸네컷을 출력하거나 다운로드하여 <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;간직하세요.</li>
+    </ul>
     </div>
     </div>
             <button className="next-button" onClick={ onComplete }>프레임 고르러 가기</button> 
         </> 
     )
 }
+
+
 
 export default TutorialScreen;
