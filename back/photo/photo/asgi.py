732f3a844ac8ed1da7263794f7ee913573cf07d5
andire120 <164429441+andire120@users.noreply.github.com>
2025-03-30 00:03:47 +0900

    d

--- back/photo/photo/asgi.py
+++ back/photo/photo/asgi.py
@@ -1,18 +1,16 @@
-# asgi.py
+"""
+ASGI config for photo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from consumers import consumers  # 소비자(consumer)를 임포트해야 합니다.
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/some_path/", consumers.MyConsumer.as_asgi()),  # WebSocket 경로와 소비자 연결
-        ])
-    ),
-})
+application = get_asgi_application()
--- back/photo/photo/settings.py
+++ back/photo/photo/settings.py
@@ -44,14 +44,12 @@ INSTALLED_APPS = [
     'catalog.apps.CatalogConfig',
     'rest_framework',
     'rest_framework.authtoken',
-    'channels',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
